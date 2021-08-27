49d00b2d081c24b411575c5db6c0c0da295eea9d
brandonHafley <bchafley@gmail.com>
2021-08-27 09:03:40 -0500

    Include Circle class in Area class

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -10,6 +10,6 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
         radius = input.nextDouble();
-        System.out.println("The area of a circle of radius " + radius + " is: " + (Math.PI * radius * radius));
+        System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
     }
 }
