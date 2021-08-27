b8609e6149a04a3df6037774680b9b5ea73afce5
brandonHafley <bchafley@gmail.com>
2021-08-27 14:58:11 -0500

    Bonus Missions

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -9,7 +9,22 @@ public class Area {
         double radius;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
-        radius = input.nextDouble();
+
+        if (input.hasNextDouble()) {
+            radius = input.nextDouble();
+
+            while (radius <= 0) {
+                System.out.println("Enter a radius: ");
+                radius = input.nextDouble();
+            }
+
+        } else {
+            System.out.println("Please enter a positive number next time");
+            input.close();
+            return;
+        }
+
         System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
+        input.close();
     }
 }
