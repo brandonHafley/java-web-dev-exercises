b6124f6b68a2cfff0d1f80fd690848d4a14dcec2
brandonHafley <bchafley@gmail.com>
2021-08-30 22:55:06 -0500

    Add comments and tweak a line of code

--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -8,12 +8,18 @@ public class CountingCharacters {
 
     public static void main(String[] args) {
 
+        // Prompts user for a string input
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a string to count each character: ");
+
+        /* String is then made to lowercase and remove all non-alphabetical characters with regex to complete
+           bonus mission objectives. */
         String userInput = input.nextLine().toLowerCase().replaceAll("[^a-zA-Z]", "");
 
         Map<Character, Integer> characterCounts = new HashMap<>();
 
+        /* With created hashmap above, loop through each remaining character and count each occurrence
+           after it being initialized */
         for (Character c : userInput.toCharArray()) {
             if (characterCounts.containsKey(c)) {
                 characterCounts.put(c, characterCounts.get(c) + 1);
@@ -22,8 +28,9 @@ public class CountingCharacters {
             }
         }
 
+        // Display the hashmap
         for (Map.Entry<Character, Integer> element : characterCounts.entrySet()) {
-            System.out.println(element.getKey() + " - " + element.getValue());
+            System.out.println(element.getKey() + ": " + element.getValue());
         }
     }
 }
